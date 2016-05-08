165327bd466203488b88c8cb3bf5e5e13069486f
Marc-Andre Servant <marc.andre3@hotmail.com>
2016-05-08 11:01:21 -0400

    Added test for timer

--- Firmware/firmware.cpp
+++ Firmware/firmware.cpp
@@ -2,17 +2,20 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "AnalogAccel.h"
+#include "Timer.h"
 
 int main()
 {
 	sei();
 
 	AnalogAccel analogAccel;
+	Timer timer(1000); //Count increases 1000x per second (every 1 ms)
+
 	DDRB |= _BV(DDB5);
 
 	while(true)
 	{
-		if(analogAccel.getZ() > 512)
+		if((timer.getCount() < 10000) != (analogAccel.getZ() > 512))
 			PORTB |= _BV(PORTB5);
 		else
 			PORTB &= ~_BV(PORTB5);
